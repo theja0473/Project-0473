ba25b378520d5db3275cf2f44800d8bde8b24de3
theja0473 <theja0473@gmail.com>
2016-05-05 15:57:36 +0530

    UPDATE WITH NEW FILE5

---
+++ Update.sh
@@ -0,0 +1,11 @@
+#/bin/sh
+
+git add .
+git commit -m "$1"
+echo"git remote add origin https://github.com/theja0473/Project-0473.git"
+git remote add origin https://github.com/theja0473/Project-0473.git
+
+
+echo "git push -u origin master";
+
+
